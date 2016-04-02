7ab15abc5661e9fa692d6ffed9da5ef67b1a6eb1
Daniel Sprouse <dwsprouse@gmail.com>
2016-04-02 12:25:08 -0400

    Updates to sidebar and Facebook

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
-	// $.ajaxSetup({ cache: true });
-	// $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
-	// FB.init({
-	// 	appId: '{your-app-id}',
-	// 	version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
-	// });     
-	// $('#loginbutton,#feedbutton').removeAttr('disabled');
-	// FB.getLoginStatus(updateStatusCallback);
- //  });
+
 	var divs = $('div[id^="option-"]').hide(),
 	    i = 0;
 	(function cycle() { 
